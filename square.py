be73a1c6b3bab74fe474f39df986e471fd9f5bdb
jyo-022 <jyothikrishna2207@gmail.com>
2025-05-20 19:35:11 +0530

    Delete square.py

--- square.py
+++
@@ -1,7 +0,0 @@
- # Create a list of numbers
-numbers = [1, 2, 3, 4, 5]
-
-# Print the square of each number
-for num in numbers:
-    square = num * num
-    print(f"The square of {num} is {square}")
